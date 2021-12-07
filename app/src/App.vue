5dc2d3aa51cc85f3d1ebd459f00971fcc0a56d7d
ilmoi <iljamoi@protonmail.com>
2021-12-07 09:14:48 +0200

    added vue app boilerplate

---
+++ app/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <!--<div>-->
+  <!--  <router-link to="/">Home</router-link> |-->
+  <!--</div>-->
+  <router-view />
+</template>
+
+<style></style>
---
+++ app/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="mt-2">hello world</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
