b3704019b37bfce3dfdc550d54b139d4e810244c
ilmoi <iljamoi@protonmail.com>
2021-12-14 09:56:49 +0200

    temp solution for errors - single enum in common lib w/ no reordering. will revisit later

--- lib/gem_common/src/errors.rs
+++ lib/gem_common/src/errors.rs
@@ -1,5 +1,14 @@
 use anchor_lang::prelude::*;
 
+/// Do NOT reorder the errors in this enum. Tests are relying on error ordering.
+/// Not great, but for some reason when ErrorCode is factored out into a lib,
+/// test messages no longer print actual messages and print error codes instead.
+///
+/// The other alternative is to have a custom error type inside the common library
+/// and to try to convert that -> ErrorCode -> ProgramError
+/// Unfortunately I wasn't able to get that working, last leg is failing.
+///
+/// todo to revisit in v1
 #[error]
 pub enum ErrorCode {
     //0
