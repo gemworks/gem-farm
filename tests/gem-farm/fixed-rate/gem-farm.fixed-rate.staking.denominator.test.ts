7c45e2b68c618ec014123eb797678ea05cef461d
David Shaw <dshaw002@gmail.com>
2022-03-02 23:47:23 +0000

    Updates test to check for time updates w/ denominator

--- tests/gem-farm/fixed-rate/gem-farm.fixed-rate.staking.denominator.test.ts
+++ tests/gem-farm/fixed-rate/gem-farm.fixed-rate.staking.denominator.test.ts
@@ -19,11 +19,11 @@ import { createMetadata } from '../../metaplex';
 
 chai.use(chaiAsPromised);
 
-describe('staking (fixed rate)', () => {
+describe('staking (fixed rate w/ denominator)', () => {
   let gf = new GemFarmTester();
 
   beforeEach('preps accs', async () => {
-    await gf.prepAccounts(5000000000);
+    await gf.prepAccounts(5000000000, 1, 1);
     await gf.callInitFarm(defaultFarmConfig, RewardType.Fixed);
     await gf.prepGemRarities();
     await gf.callInitFarmer(gf.farmer1Identity);
@@ -31,74 +31,69 @@ describe('staking (fixed rate)', () => {
     await gf.callDeposit(gf.gem1Amount, gf.farmer1Identity);
     await gf.callDeposit(gf.gem2Amount, gf.farmer2Identity);
     await gf.callAuthorize();
-    await gf.callFundReward(undefined, fixedConfigWithDenominator,);
+    await gf.callFundReward(undefined, fixedConfigWithDenominator);
   });
 
-  it('stakes -> accrues -> claims (multi farmer) -> ONLY ONE DAY -> NEW!', async () => {
+  it('stakes -> refresh before first reward (2s) -> lastUpdatedTs no change -> refresh after first reward (14s) -> lastUpdatedTs change (10s) -> refresh farmer (18s) -> no change in lastUpdatedTs(10s)', async () => {
     // ----------------- stake + accrue
-    await gf.stakeAndVerify(gf.farmer1Identity);
-    await gf.stakeAndVerify(gf.farmer2Identity);
+    const farmer1 = await gf.stakeAndVerify(gf.farmer1Identity);
+    const farmer2 = await gf.stakeAndVerify(gf.farmer2Identity);
 
-    await pause(11000);
+    let lastUpdatedTs1 = new BN(
+      gf.reward === 'rewardA' ?
+      farmer1.rewardA.fixedRate['lastUpdatedTs'] :
+      farmer1.rewardB.fixedRate['lastUpdatedTs']
+    );
+    let lastUpdatedTs2 = new BN(
+      gf.reward === 'rewardA' ?
+      farmer2.rewardA.fixedRate['lastUpdatedTs'] :
+      farmer2.rewardB.fixedRate['lastUpdatedTs']
+    );
 
-    //manually refresh to update accrued rewards for each farmer
+    await pause(2000); // 2s
+
     await gf.callRefreshFarmer(gf.farmer1Identity);
     await gf.callRefreshFarmer(gf.farmer2Identity);
 
-    //verify counts
+    let f1Reward = await gf.verifyFarmerReward(gf.farmer1Identity);
+    let f2Reward = await gf.verifyFarmerReward(gf.farmer2Identity);
+
     await gf.verifyStakedGemsAndFarmers(2);
 
-    //verify funds
-    const f1Reward = await gf.verifyFarmerReward(gf.farmer1Identity);
-    const f2Reward = await gf.verifyFarmerReward(gf.farmer2Identity);
-    assert(f1Reward.accruedReward.eq(gf.gem1Amount.mul(fixedConfigWithDenominator.schedule.baseRate)));
-    assert(f2Reward.accruedReward.eq(gf.gem2Amount.mul(fixedConfigWithDenominator.schedule.baseRate)));
+    // lastUpdated should be the same because it's before denominator
+    assert(lastUpdatedTs1.eq(f1Reward.fixedRate.lastUpdatedTs));
+    assert(lastUpdatedTs2.eq(f2Reward.fixedRate.lastUpdatedTs));
 
-    console.log("Running verifyFarmerFixedRewardTimings");
+    await pause(12000); // 14s
+
+    await gf.callRefreshFarmer(gf.farmer1Identity);
+    await gf.callRefreshFarmer(gf.farmer2Identity);
+
+    //verify funds
+    f1Reward = await gf.verifyFarmerReward(gf.farmer1Identity);
+    f2Reward = await gf.verifyFarmerReward(gf.farmer2Identity);
+
+    // the time should be updated now to just previous time + denominator (not +16s)
+    assert(lastUpdatedTs1.eq(f1Reward.fixedRate.lastUpdatedTs.sub(fixedConfigWithDenominator.schedule.denominator)));
+    assert(lastUpdatedTs2.eq(f2Reward.fixedRate.lastUpdatedTs.sub(fixedConfigWithDenominator.schedule.denominator)));
     
+    lastUpdatedTs1 = f1Reward.fixedRate.lastUpdatedTs;
+    lastUpdatedTs2 = f2Reward.fixedRate.lastUpdatedTs;
+
+    await pause(4000); // 18s
+
+    f1Reward = await gf.verifyFarmerReward(gf.farmer1Identity);
+    f2Reward = await gf.verifyFarmerReward(gf.farmer2Identity);
+
+    // time should be the same as before
+    assert(lastUpdatedTs1.eq(f1Reward.fixedRate.lastUpdatedTs));
+    assert(lastUpdatedTs2.eq(f2Reward.fixedRate.lastUpdatedTs));
+
     // ----------------- claim
     await gf.callClaimRewards(gf.farmer1Identity);
     await gf.callClaimRewards(gf.farmer2Identity);
 
     // see that the reward is only equal to one interval reward...
-    const farmer1Reward = await gf.verifyFarmerReward(gf.farmer1Identity);
-    const paidout = new BN(gf.gem1Amount.mul(fixedConfigWithDenominator.schedule.baseRate));
-    assert(paidout.eq(farmer1Reward.paidOutReward));
-
-    await gf.verifyClaimedReward(gf.farmer1Identity);
-    await gf.verifyClaimedReward(gf.farmer2Identity);
-  });
-
-  it('Multi farmer.. stake -> accure (but not enough) -> attempt to claim before payout period -> get nothing!', async () => {
-    // ----------------- stake + accrue
-    await gf.stakeAndVerify(gf.farmer1Identity);
-    await gf.stakeAndVerify(gf.farmer2Identity);
-
-    await pause(5000); // 5s
-
-    //manually refresh to update accrued rewards for each farmer
-    await gf.callRefreshFarmer(gf.farmer1Identity);
-    await gf.callRefreshFarmer(gf.farmer2Identity);
-
-    //verify counts
-    await gf.verifyStakedGemsAndFarmers(2);
-
-    //verify funds
-    //await gf.verifyAccruedRewardsFixedsd();
-    const f1Reward = await gf.verifyFarmerReward(gf.farmer1Identity);
-    const f2Reward = await gf.verifyFarmerReward(gf.farmer2Identity);
-    assert(f1Reward.accruedReward.eq(new BN(0)));
-    assert(f2Reward.accruedReward.eq(new BN(0)));
-
-    // ----------------- claim
-    await gf.callClaimRewards(gf.farmer1Identity);
-    await gf.callClaimRewards(gf.farmer2Identity);
-
-    // see that the reward is only equal to one minute reward...
-    const farmer1Reward = await gf.verifyFarmerReward(gf.farmer1Identity);
-    const paidout = new BN(0);
-    assert(paidout.eq(farmer1Reward.paidOutReward));
-
     await gf.verifyClaimedReward(gf.farmer1Identity);
     await gf.verifyClaimedReward(gf.farmer2Identity);
   });
