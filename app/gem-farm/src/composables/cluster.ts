66e3ee0c29f4e3debb99683b2f0ea2bdeed4621b
ilmoi <iljamoi@protonmail.com>
2022-04-13 13:14:56 +0300

    move to public cluster

--- app/gem-farm/src/composables/cluster.ts
+++ app/gem-farm/src/composables/cluster.ts
@@ -9,7 +9,8 @@ export enum Cluster {
 }
 
 const clusterURLMapping = {
-  mainnet: process.env.VUE_APP_MAINNET_URL || 'https://ssc-dao.genesysgo.net/',
+  mainnet:
+    process.env.VUE_APP_MAINNET_URL || 'https://api.mainnet-beta.solana.com',
   devnet: process.env.VUE_APP_DEVNET_URL || 'https://api.devnet.solana.com',
   testnet: process.env.VUE_APP_TESTNET_URL || 'https://api.testnet.solana.com',
   localnet: process.env.VUE_APP_LOCALNET_URL || 'http://localhost:8899',
