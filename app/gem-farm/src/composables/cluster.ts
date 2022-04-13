f64d4606d757e4b5fb37440e6b6a740353131793
ilmoi <iljamoi@protonmail.com>
2022-04-13 13:22:36 +0300

    move to gengo authed

--- app/gem-farm/src/composables/cluster.ts
+++ app/gem-farm/src/composables/cluster.ts
@@ -1,5 +1,7 @@
 import { readonly, ref } from 'vue';
 import { Commitment, Connection } from '@solana/web3.js';
+import { tokenAuthFetchMiddleware } from '@strata-foundation/web3-token-auth';
+import axios from "axios";
 
 export enum Cluster {
   Mainnet = 'mainnet',
@@ -21,15 +23,23 @@ const cluster = ref<Cluster>(Cluster.Mainnet);
 export default function useCluster() {
   const getClusterURL = (): string => clusterURLMapping[cluster.value];
 
-  const getConnection = (committment?: Commitment): Connection =>
-    new Connection(getClusterURL(), committment ?? 'processed');
+  const getToken = async (): Promise<string> =>
+    // @ts-ignore
+    (await axios.get(process.env.VUE_APP_GENGO_AUTH)).data.access_token;
+
+  const getConnection = (commitment?: Commitment): Connection =>
+    new Connection(getClusterURL(), {
+      commitment: commitment ?? 'processed',
+      fetchMiddleware: tokenAuthFetchMiddleware({
+        // tokenExpiry: 0,
+        getToken,
+      }),
+    });
 
   const setCluster = (newCluster: Cluster) => {
     cluster.value = newCluster;
-    // capping at 20 chars due to security (not to expose the token)
-    console.log(
-      `Cluster updated,  now ${newCluster} (${getClusterURL().substr(0, 20)})`
-    );
+    // capping at 10 chars due to security (not to expose the token)
+    console.log(`Cluster updated, now ${newCluster} (${getClusterURL().substr(0, 10)})`);
   };
 
   return {
