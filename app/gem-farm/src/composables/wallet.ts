704dac1d7ffc3d3f874c94ecba679542e63043b4
Ander Ustarroz <anderustarroz@gmail.com>
2023-02-16 18:28:01 +1030

    Fixed frontend

--- app/gem-farm/src/composables/wallet.ts
+++ app/gem-farm/src/composables/wallet.ts
@@ -1,25 +1,26 @@
 import { computed, readonly, ref, shallowRef, Ref } from 'vue';
 import {
-  getPhantomWallet,
-  getSolflareWallet,
-  getSolflareWebWallet,
-  getSolletExtensionWallet,
-  getSolletWallet,
-  Wallet,
-  WalletName,
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  // Wallet,
 } from '@solana/wallet-adapter-wallets';
 import { PublicKey } from '@solana/web3.js';
-import { SignerWalletAdapter } from '@solana/wallet-adapter-base';
+import {
+  SignerWalletAdapter,
+  WalletName,
+  WalletAdapter as Wallet,
+} from '@solana/wallet-adapter-base';
 
 const walletClass = ref<Wallet | null>(null);
 const walletAdapter = ref<Ref<SignerWalletAdapter | null>>(shallowRef(null));
 
 const walletMapping = {
-  Phantom: getPhantomWallet,
-  Sollet: getSolletWallet,
-  'Sollet (Extension)': getSolletExtensionWallet,
-  Solflare: getSolflareWallet,
-  'Solflare (Web)': getSolflareWebWallet,
+  Phantom: PhantomWalletAdapter,
+  Sollet: SolletWalletAdapter,
+  SolletExtension: SolletExtensionWalletAdapter,
+  Solflare: SolflareWalletAdapter,
 };
 
 export default function useWallet() {
@@ -34,16 +35,16 @@ export default function useWallet() {
 
   const setWallet = (newWallet: string | null, network: string) => {
     console.log('attempting to set wallet', newWallet, network.substr(0, 20));
-
     if (!newWallet) {
       console.log('removing active wallet');
       walletClass.value = null;
       walletAdapter.value = null; // don't think I need shallowRef here
       return;
     }
-
-    const gottenWallet = (walletMapping as any)[newWallet!]({ network });
-    const connectedAdapter = gottenWallet.adapter();
+    const gottenWallet = (walletMapping as any)[newWallet!];
+    const connectedAdapter = new (walletMapping as any)[newWallet!]({
+      network,
+    });
     connectedAdapter
       .connect()
       .then(() => {
