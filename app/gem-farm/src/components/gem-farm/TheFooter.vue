0e9ac3475348a29a8b72071df4ab4c015dbcee25
Hank Chen <hank@Hanks-MacBook-Pro.local>
2023-05-04 12:49:48 -0400

    fix3

--- app/gem-farm/src/components/gem-farm/TheFooter.vue
+++ app/gem-farm/src/components/gem-farm/TheFooter.vue
@@ -2,7 +2,6 @@
   <div class="pt-1 pb-1 footer">
     <p class="px-3 pt-1">Built with</p>
     <i class="nes-icon heart"></i>
-    <img src="../../assets/helius.png" height="31" />
   </div>
 </template>
 
